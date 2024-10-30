4f3d00ede9a0b7af0ddec1a2201f65d5cd55c3d4
josias751 <josiasbarrera958@gmail.com>
2024-10-29 22:16:45 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function changeImageOver()
+{
+    document.getElementById("cartoon_img").src ="https://c7889ed4fe666574755dd2115c93a66e0.gif";
+}
+
+function changeImageLeave()
+{
+    document.getElementById("cartoon_img").src ="hhtps://4a868dc6ec2b3e25b5cd306cde76ebab.gif";
+}
